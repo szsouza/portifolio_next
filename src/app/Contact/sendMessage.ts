ccd3a83c64f16583f23ee5940dba12df754552ed
Rodrigo <rodrigosouzaprof.ff@gmail.com>
2024-03-20 23:42:49 -0300

    ajuste no na funcao usando nodemailer

--- src/app/Contact/sendMessage.ts
+++ src/app/Contact/sendMessage.ts
@@ -1,44 +1,34 @@
-'use server'
-import nodemailer from 'nodemailer';
+"use server";
+import nodemailer from "nodemailer";
 
 async function sendMessage(formData: FormData) {
- 
-    const rawFormData = {
-      nome: formData.get('nome'),
-      email: formData.get('email'),
-      mensagem: formData.get('mensagem'),
-    }
-    try {
-        const transporter = nodemailer.createTransport({
-          host: "smtp.gmail.com",
-          service: "Gmail",
-          port: 465,
-          secure: false,
-          auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASSWORD
-          }
-        });
-  
-        await transporter.sendMail({
-          from: process.env.EMAIL_USER,
-          to: process.env.RECIPIENT_EMAIL,
-          subject: 'contact',
-          text: `Name: ${rawFormData.nome}\nEmail: ${rawFormData}\nMessage: ${rawFormData.mensagem}`
-        });
-        console.log(rawFormData)
-  return
-      } catch (error) {
-        console.error(error);
-       
-      }    
+  const rawFormData = {
+    nome: formData.get("nome"),
+    email: formData.get("email"),
+    mensagem: formData.get("mensagem"),
+  };
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp-mail.outlook.com",
+      port: 587,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: process.env.RECIPIENT_EMAIL,
+      subject: "contact",
+      text: `Name: ${rawFormData.nome}\nEmail: ${rawFormData.email}\nMessage: ${rawFormData.mensagem}`,
+    });
+    console.log(rawFormData);
+    return;
+  } catch (error) {
+    console.error(error);
   }
+}
 
-  export {sendMessage}
-
-
-
- 
-
-    
-
+export { sendMessage };
